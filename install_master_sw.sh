080bbf0381041feef74235d58d8c1a6969b81d46
Anil <amawsdo@gmail.com>
2022-05-16 20:48:18 -0400

    master server user data script file

---
+++ install_master_sw.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+touch /a
+sudo dnf install -y gcc
+sudo dnf install -y openssl-devel
+sudo dnf install -y zlib-devel
+sudo dnf install -y unixODBC-devel
+sudo dnf install -y ncurses-devel
+sudo dnf install -y bzip2-devel
+sudo dnf install -y git
+sudo dnf install -y vim-enhanced
+touch /b
